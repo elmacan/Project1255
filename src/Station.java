ce1c38623a98722cde955e2a45269c06e0388b08
AlaraBurce <alarahan9@gmail.com>
2024-05-08 19:09:12 +0300

    working on station

--- src/Station.java
+++ src/Station.java
@@ -2,9 +2,23 @@ public class Station {
     private String stationID;
     private int maxCapacity;
     private double speed;
-    //private String multiFlag;
-    //private String fifoFlag;
-    //private double plusMinus;
+    private String multiFlag;
+    private String fifoFlag;
+    private double plusMinus;
 
-
+    public Station (String stationID, int maxCapacity, double speed){ //get set for values
+        this.stationID = stationID;
+        this.maxCapacity = maxCapacity;
+        this.speed = speed;
+    }
+    public String getStationID(){
+        return this.stationID;
+    }
+    public int getMaxCapacity(){
+        return this.maxCapacity;
+    }
+    public double getSpeed(){
+        return this.speed;
+    }
+    pub
 }
