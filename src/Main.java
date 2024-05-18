bc119f22852dcfc1b22ad3ddd8650f9d39210f33
elmacan <betozsan@hotmail.com>
2024-05-18 03:38:48 +0300

    merge on master

--- src/Main.java
+++ src/Main.java
@@ -19,261 +19,31 @@ public class Main {
         }
         String workFileName = args[0];
         String jobFileName = args[1];
-        ArrayList<Job> jobTypesInText = new ArrayList<Job>(); //job objelerini içeren arraylist
-        ArrayList<String> taskTypesInText = new ArrayList<String>();  //task objelerini içeren list
-        fileControl(workFileName, jobFileName, taskTypesInText, jobTypesInText);
+
+        Validator validator=new Validator();
+        validator.fileControl(workFileName,jobFileName);
+
+        FileParser fileParser = new FileParser();
 
 
-        for (int i = 0; i < jobTypesInText.size(); i++) {
+        //validator.getErrors().add("T5 has a negative task size.");
 
-            jobTypesInText.get(i).printTasks();
-        }
-       // jobTypesInText.get(0).printTasks();
-
-        int currentTime = 0;
-
-        //event queue yapılcak
-
-
-    }
-
-    public static void fileControl(String workFileName, String jobFileName,ArrayList<String> taskTypesInText,ArrayList<Job> jobTypesInText) {
-        File workFlowFile = new File(workFileName);
-        File jobFile = new File(jobFileName);
-
-        boolean errorOccured = false;
-        if(isCorrectWorkFileFormat(workFlowFile,taskTypesInText,jobTypesInText)) {
-            System.out.println("Correct WorkFlowFile Format");
-        }else System.out.println("Wrong WorkFlowFile Format ");
-
-
-        try {
-            if (!workFlowFile.exists() || !workFlowFile.canRead()) {
-                throw new IOException(workFileName + " doesn't exists or is not readable!");
-
+        // Checking for errors
+        if (validator.hasErrors()) {
+            System.out.println("Errors occurred:");
+            for (String error : validator.getErrors()) {
+                System.out.println(error);
             }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            errorOccured = true;
-
-        }
-        try {
-            if (!jobFile.exists() || !jobFile.canRead()) {
-                throw new IOException(jobFileName + " doesn't exists or is not readable!");
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            errorOccured = true;
-
-        }
-
-        if (errorOccured == true) {
             System.exit(1);
-        }
-
-
-
-    }
-
-
-    //BET exception olarak çevir
-    //BET description error örneklerine göre bak
-    //BET human readable versiyonu ekle
-    //BET errorların kaçıncı satırda olduğunu yazdır
-    //BET task da falan yanlış format varsa sonraki yanlışları göstermiyor o düzeltilcek
-
-    public static boolean isCorrectWorkFileFormat(File workFlowFile,ArrayList<String> taskTypesInText,ArrayList<Job> jobTypesInText){    //tüm line ın sayı harf _ . dan oluşmasını parantezlerin ve title ların yerlerini kontrol ediyor
-
-           // ArrayList<String>stationInfoInText=new ArrayList<String>();
-            int countIndex=0;
-            boolean taskTypesFound = false;
-            boolean jobTypesFound = false;
-            boolean stationsFound = false;
-            Scanner workScanner = null;
-
-            try {
-                workScanner = new Scanner(workFlowFile);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-
-            String line;
-            //workfile dosyasını okuyan scanner objesi
-            while (workScanner.hasNextLine()) {
-                line = workScanner.nextLine();
-                System.out.println("Line: " + line);
-                //BET kaçıncı line da hata verdiğini bul
-
-                if (line.startsWith("(TASKTYPES")) {
-                    String[] pieces=line.split(" ");
-                    parseTaskTypes(pieces,taskTypesInText);
-                    String splittedline=line.replaceAll("\\s", "");//boşlukları çıkarıyor
-                    taskTypesFound = true;
-                    if (!splittedline.matches("^\\(TASKTYPES(\\w[.]?)*\\)$")) {
-                        System.out.println("Error: Invalid format in TASKTYPES section.");
-                        return false;
-                    }
-                    continue;
-                } else if (line.equals("(JOBTYPES")) {
-
-                    while(!(line.startsWith("(STATIONS"))) {
-                        line = workScanner.nextLine();
-                        String[] pieces=line.split(" ");
-                        parseJobTypes(pieces,jobTypesInText,taskTypesInText,countIndex);
-                        countIndex++;
-                        System.out.println("line: "+line);
-                        String splittedline = line.replaceAll("\\s", "");//boşlukları çıkarıyor
-                        if(splittedline.matches("^\\((\\w[.]?)*\\)\\)$"))break;
-                        jobTypesFound = true;
-                        if ( !((splittedline.matches("^\\((\\w[.]?)*\\)$")) || (splittedline.matches("^\\((\\w[.]?)*\\)\\)$")) )) {
-                            System.out.println("Error: Invalid format in JOBTYPES section.");
-                            return false;
-                        }
-
-                    }
-                    continue;
-                } else if (line.equals("(STATIONS")) {
-                    line=workScanner.nextLine();
-                    System.out.println("line: "+line);
-                    stationsFound = true;
-                    String splittedline = line.replaceAll("\\s", "");//boşlukları çıkarıyor
-
-                    if (!splittedline.matches("^\\((\\w[.]?)*\\)$")) {
-                        System.out.println("Error: Invalid format in STATIONS section.");
-                        return false;
-                    }
-                }
-
-                if (!taskTypesFound) {
-                    System.out.println("Error: TASKTYPES section not found.");
-                    return false;
-                }
-                if (!jobTypesFound) {
-                    System.out.println("Error: JOBTYPES section not found.");
-                    return false;
-                }
-                if (!stationsFound) {
-                    System.out.println("Error: STATIONS section not found.");
-                    return false;
-                }
-
-
-            }
-            return true;
-    }
-
-    public static void parseJobTypes(String[] pieces,ArrayList<Job> jobTypesInText,ArrayList<String> taskTypesInText,int countIndex) {
-        ArrayList<String> realPieces = new ArrayList<String>();
-        Validator validator = new Validator();
-
-        for (String s : pieces) {
-            if (!s.contains("(") && !s.contains(")")) {
-                realPieces.add(s);
-            }
-        }
-
-        for (String s : realPieces) {
-            System.out.println("pieces: " + s);
-        }
-
-        /*Job job = new Job();
-        if (validator.isValidID(realPieces.get(0))) {
-            job.setJobType(realPieces.get(0));
-            jobTypesInText.add(job);
         } else {
-            System.out.println("invalid jobtypeId");
-            System.exit(1);
-
-        }*/
-
-        int j = 0;
-        //BET declare olmamış task var mı
-        for (int i = 1; i < realPieces.size(); i++) {
-            if (!validator.isNumber(realPieces.get(i))) { //t1 e bakan
-                if (validator.isValidID(realPieces.get(i))) {
-                    Task task = new Task();
-                    task.setTaskType(realPieces.get(i));
-                    job.getTasks().add(task);
-
-                    if ((i + 1 < realPieces.size()) && (validator.isNumber(realPieces.get(i + 1)))) {
-
-                        task.setTaskSize(Double.parseDouble(realPieces.get(i + 1)));
-                        //job.getTasks().add(task);
-                    } else {
-
-                        if (findDefaultSizeOfTaskType(realPieces.get(i), taskTypesInText) == -1) {
-                            System.out.println(realPieces.get(i) + " has no size");
-
-                        } else {
-
-                            task.setTaskSize(findDefaultSizeOfTaskType(realPieces.get(i), taskTypesInText));
-                            //job.getTasks().add(task);
-                        }
-                    }
-
-                }
-            }
-
+            System.out.println("No errors detected.");
         }
 
-
-    }
-
-    public static double findDefaultSizeOfTaskType(String taskTypeID, ArrayList<String> taskTypesInText) {
-        Validator validator = new Validator();
-
-        for (int i = 0; i < taskTypesInText.size(); i++) {
-
-            if (taskTypeID.equals(taskTypesInText.get(i))) {
-
-                if (i + 1 < taskTypesInText.size() && validator.isNumber(taskTypesInText.get(i + 1))) {
-
-                    return Double.parseDouble(taskTypesInText.get(i + 1));
-                }
-            }
-        }
-        return -1;
-    }
-
-
-
-
-
-    //BET stringbuilder bak bir
-    //BET parantez öncesi boşluk olmazsa son elemanı almıyor onu sonra düzelt
-    public static void parseTaskTypes(String[] pieces,ArrayList<String> taskTypesInText) {
-        Validator validator = new Validator();
-
-
-
-
-        for(String s: pieces){
-            if (!s.contains("(") && !s.contains(")") && !s.contains("(TASKTYPES")) {
-                taskTypesInText.add(s);
-            }
-        }
-
-        for(String s: taskTypesInText){
-           System.out.println("pieces: "+s);
-        }
-
-        if (validator.isNumber(taskTypesInText.get(0))) {
-            System.out.println("Error: task line sayıyla başlıyor");
-            //BET error verince sistemi kapat
-        }
+        //fileParser.printFile();
 
 
 
     }
-
-
-
-
-
-
-
-
-
 
 
 }
